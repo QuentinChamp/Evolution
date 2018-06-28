4a3bde3f6f69633c44ffe1e4918e613eabd8556d
Unknown <guillaume.p.dumas@gmail.com>
2018-06-28 16:55:11 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/evolution/Ville.java
+++ src/evolution/Ville.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Ville {
-	
+
 	private byte compteur=0;
 	private byte id;
 	private String nom;
@@ -32,13 +32,25 @@ public class Ville {
 	public String getNom() {
 		return nom;
 	}
-
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
 	
 	public ArrayList<Batiment> getBatiment() {
 		return this.batiments;
+	}
+	
+	public void proprioVille(Joueur joueur) { // verifie si tous les batiments d'une ville appartiennent au joueur, si oui la ville appartient au joueur
+		
+		int compteurEtat=0;
+		
+		for(Batiment b : this.batiments)
+		{
+			if(b.getEtat().equals(joueur.getNom()))
+					compteurEtat++;
+		}
+		
+		if(compteurEtat == this.batiments.size())
+			this.etat = joueur.getNom();
+		else
+			this.etat = "";
 	}
 	
 }
