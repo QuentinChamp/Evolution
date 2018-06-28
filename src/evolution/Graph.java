00f43204b78cb957892b9f937f96da1373b57a01
QuentinChamp <champagne.quentin@gmail.com>
2018-06-28 13:34:39 +0200

    graph....

---
+++ src/evolution/Graph.java
@@ -0,0 +1,58 @@
+package evolution;
+
+public class Graph {
+
+	public static void Graph() {
+		System.out.println("                  O                                         ");
+		System.out.println("                  |                                         ");
+		System.out.println("        __________|_________                               ");
+		System.out.println("       |                    |                               ");
+		System.out.println("       |     _         _    |                               ");
+		System.out.println("       |    |_|       |_|   |                               ");
+		System.out.println("       |     _         _    |                               ");
+		System.out.println("       |    |_|       |_|   |                               ");
+		System.out.println("       |     _         _    |____||____                     ");
+		System.out.println("       |    |_|       |_|   |          |                    ");
+		System.out.println("       |     _         _    |   o   o  |                  /|");
+		System.out.println("       |    |_|       |_|   |   o   o  |                 /| ");
+		System.out.println("       |     _         _    |   o   o  |                /|  ");
+		System.out.println("       |    |_|       |_|   |   o   o  |               /|   ");
+		System.out.println("       |     _         _    |   o   o  |              /|    ");
+		System.out.println("       |    |_|       |_|   |   o   o  |             /|     ");
+		System.out.println("       |     _         _    |   o   o  |            /|      ");
+		System.out.println("       |    |_|       |_|   |   o   o  |           /|       ");
+		System.out.println("       |     _         _    |   o   o  |          /|        ");
+		System.out.println("       |    |_|       |_|   |   o   o  |         /|         ");
+		System.out.println("       |     _         _    |   o   o  |        /|          ");
+		System.out.println("       |    |_|       |_|   |   o   o  |       /|           ");
+		System.out.println("       |     _         _    |   o   o  |      /|            ");
+		System.out.println("       |    |_|       |_|   |   o   o  |     /|             ");
+		System.out.println("       |         ___        |   o   o  |    /|              ");
+		System.out.println("       |        |   |       |__________|   /|               ");
+		System.out.println("       |________|___|_______|                               ");
+		
+		
+	}
+	
+	public static void evolutionIntro() {
+		
+		System.out.println("EEEEEEEEEEEEEEEEEEEEEE                                        lllllll 	                       tttt            iiii                   	          ");
+		System.out.println("E::::::::::::::::::::E                                        l:::::l 	                    ttt:::t           i::::i                  	         ");
+		System.out.println("E::::::::::::::::::::E                                        l:::::l                       t:::::t            iiii                   	     ");
+		System.out.println("EE::::::EEEEEEEEE::::E                                        l:::::l                       t:::::t                                   nnnn  nnnnnnnn");
+		System.out.println("  E:::::E       EEEEEEvvvvvvv           vvvvvvv ooooooooooo    l::::l uuuuuu    uuuuuuttttttt:::::ttttttt    iiiiiii    ooooooooooo   n:::nn::::::::nn");
+		System.out.println("  E:::::E              v:::::v         v:::::voo:::::::::::oo  l::::l u::::u    u::::ut:::::::::::::::::t    i:::::i  oo:::::::::::oo n::::::::::::::nn");
+		System.out.println("  E::::::EEEEEEEEEE     v:::::v       v:::::vo:::::::::::::::o l::::l u::::u    u::::ut:::::::::::::::::t     i::::i o:::::::::::::::onn:::::::::::::::n");
+		System.out.println("  E:::::::::::::::E      v:::::v     v:::::v o:::::ooooo:::::o l::::l u::::u    u::::utttttt:::::::tttttt     i::::i o:::::ooooo:::::o  n:::::nnnn:::::n");
+		System.out.println("  E:::::::::::::::E       v:::::v   v:::::v  o::::o     o::::o l::::l u::::u    u::::u      t:::::t           i::::i o::::o     o::::o  n::::n    n::::n");
+		System.out.println("  E::::::EEEEEEEEEE        v:::::v v:::::v   o::::o     o::::o l::::l u::::u    u::::u      t:::::t           i::::i o::::o     o::::o  n::::n    n::::n");
+		System.out.println("  E:::::E                   v:::::v:::::v    o::::o     o::::o l::::l u::::u    u::::u      t:::::t           i::::i o::::o     o::::o  n::::n    n::::n");
+		System.out.println("  E:::::E       EEEEEE       v:::::::::v     o::::o     o::::o l::::l u:::::uuuu:::::u      t:::::t    tttttt i::::i o::::o     o::::o  n::::n    n::::n");
+		System.out.println("EE::::::EEEEEEEE:::::E        v:::::::v      o:::::ooooo:::::ol::::::lu:::::::::::::::uu    t::::::tttt:::::ti::::::io:::::ooooo:::::o  n::::n    n::::n");
+		System.out.println("E::::::::::::::::::::E         v:::::v       o:::::::::::::::ol::::::l u:::::::::::::::u    tt::::::::::::::ti::::::io:::::::::::::::o	n::::n    n::::n");
+		System.out.println("E::::::::::::::::::::E          v:::v         oo:::::::::::oo l::::::l  uu::::::::uu:::u      tt:::::::::::tti::::::i oo:::::::::::oo 	n::::n    n::::n");
+		System.out.println("EEEEEEEEEEEEEEEEEEEEEE           vvv            ooooooooooo   llllllll    uuuuuuuu  uuuu        ttttttttttt  iiiiiiii   ooooooooooo   	nnnnnn    nnnnnn");
+		System.out.println("");
+		System.out.println("");
+	}
+}
